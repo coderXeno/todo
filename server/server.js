db4edd07e08a41b44a956e71b795bc80f2bf6af0
coderXeno <h.37.DiptangsuGupta@gmail.com>
2023-08-02 15:32:45 +0530

    few minor bug fixes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -12,7 +13,8 @@ app.use('/api', require("./routes/authRoute"));
 app.use('/api', require("./routes/taskRoute"));
 
 const port = 8000;
-mongoose.connect('mongodb://localhost:27017/todo-db', {
+const mongoServer = process.env.MONGO_SERVER;
+mongoose.connect(mongoServer, {
     useNewUrlParser: true
 }).then((res) => console.log("Connected to DB"))
 .catch((err) => console.log(err));
